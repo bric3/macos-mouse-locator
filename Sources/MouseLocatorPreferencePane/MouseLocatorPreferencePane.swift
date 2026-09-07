cd88bd00102d72a44199c19a2c1d596cf5a5175b
Brice Dutheil <brice.dutheil@gmail.com>
2026-09-07 11:01:38 +0200

    fix: reach preference pane storage

--- Sources/MouseLocatorPreferencePane/MouseLocatorPreferencePane.swift
+++ Sources/MouseLocatorPreferencePane/MouseLocatorPreferencePane.swift
@@ -8,11 +8,11 @@ final class MouseLocatorPreferencePane: NSPreferencePane {
     let view = MainActor.assumeIsolated {
       let view = NSHostingView(
         rootView: SettingsView(settings: .shared)
-          .frame(width: 660, height: 660)
+          .frame(width: 660, height: 760)
           .scrollDisabled(true)
           .scrollIndicators(.hidden)
       )
-      view.frame = NSRect(x: 0, y: 0, width: 660, height: 660)
+      view.frame = NSRect(x: 0, y: 0, width: 660, height: 760)
       return view
     }
     mainView = view
